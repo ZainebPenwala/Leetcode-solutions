01075ff76ee052b13d32b4051d9d33265402802f
Zaineb <zenp1520@gmail.com>
2020-05-30 14:14:02 +0530

    Update missing_number.py

--- missing_number.py
+++ missing_number.py
@@ -5,8 +5,7 @@ Example 2:
 Input: [9,6,4,2,3,5,7,0,1]  Output: 8'''
 
 
-# Solution1: Check for 1 missing number
-
+# Solution 1: find 1 missing number
 class Find:
     def noMiss(self, no):
         for x in range(-1,len(no)):
@@ -14,3 +13,16 @@ class Find:
                 return (x+1)
 obj = Find()
 obj.noMiss([3,0,1])
+
+
+# Solution 2: find all missing numbers
+class Find:
+    def noMiss(self, no):
+        missed = []
+        for x in range(-1,len(no)):
+            if(x+1) not in no:
+                missed.append(x+1)
+        return missed
+
+obj = Find()
+obj.noMiss([3,0,1,5])
