f00e96eff566871deb78c65599753947ffb54b87
Zaineb <zenp1520@gmail.com>
2020-05-31 11:53:57 +0530

    Create reverse_the_digits.py

---
+++ reverse_the_digits.py
@@ -0,0 +1,23 @@
+''' Given a 32-bit signed integer, reverse digits of an integer.
+Example 1:
+  Input: 123     Output: 321
+Example 2:
+  Input: -123    Output: -321
+Example 3:
+  Input: 120     Output: 21 '''
+ 
+ 
+  class Reverse:
+    def reverse(self, num):
+        string = str(num)
+        if '-' in string:
+            t = string.strip('-')
+            mid = t[::-1]
+            ans = int('-' + mid)
+        else:
+            mid = string[::-1]
+            ans = int(mid)
+        return ans
+    
+obj = Reverse()
+obj.reverse(120)
