ae484754b2b16344eaf0b361ae1051803240b8d8
Zaineb <zenp1520@gmail.com>
2020-06-30 10:29:15 +0530

    Update single_no.py

--- single_no.py
+++ single_no.py
@@ -3,3 +3,19 @@ Example 1:
 Input: [2,2,1]   Output: 1
 Example 2:
 Input: [4,1,2,1,2]   Output: 4 '''
+
+#Solution:
+
+class Single:
+    def single_no (self,num):
+        num1 = []
+        for elem in num:
+            if elem not in num1:
+                num1.append(elem)
+            else:
+                num1.remove(elem)
+        return num1[0]
+    
+
+num_li = Single()
+num_li.single_no([4,1,2,1,2])
