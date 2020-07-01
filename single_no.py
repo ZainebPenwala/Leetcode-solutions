3b7f4b442cd829a7cb13ede78eb0bc24c8e07f8c
Zaineb <zenp1520@gmail.com>
2020-07-01 15:54:20 +0530

    Update single_no.py

--- single_no.py
+++ single_no.py
@@ -4,7 +4,7 @@ Input: [2,2,1]   Output: 1
 Example 2:
 Input: [4,1,2,1,2]   Output: 4 '''
 
-#Solution:
+#Solution 1:
 class Single:
     def single_no (self,num):
         num1 = []
@@ -13,8 +13,17 @@ class Single:
                 num1.append(elem)
             else:
                 num1.remove(elem)
-        return num1[0]
-    
-
+        return num1[0]   
 num_li = Single()
 num_li.single_no([4,1,2,1,2])
+
+#Solution 2: uding count()
+class Single:
+    def single_no (self,num):
+        for elem in num:
+            if num.count(elem)== 1:
+                print(elem)
+            else:
+                pass
+num = Single()
+num.single_no([4,1,2,1,2])
